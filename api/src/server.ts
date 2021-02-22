feff4cff0987969c88909eecef745c4caa5b1c9e
Leonardo Furtado <leonardomf1996@gmail.com>
2021-02-22 11:00:19 -0300

    Dia 1 - Inicio da API

---
+++ api/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+   return response.json({ message: 'Hello World - NLW04' });
+})
+
+app.post('/', (request, response) => {
+   return response.json({ message: 'Dados salvos com sucesso!' });
+});
+
+app.listen(3333, () => {
+   console.log('Server is running!');
+});
